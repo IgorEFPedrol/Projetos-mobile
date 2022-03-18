3fedc71248591b6c8b9577ecefaae3f215d8b78d
Igor Enrico <igorefp@gmail.com>
2022-03-17 22:08:05 -0300

    update btn logic

--- app-cantina/src/app/home/home.page.ts
+++ app-cantina/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NumericValueAccessor } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  qtdHotdog: number = 0;
+  qtdRefri: number = 0;
+  qtdBala: number = 0;
+  qtdChocolate: number = 0;
+  resultado: number = 0;
 
   constructor() {}
 
+  addHotdog() {
+    this.qtdHotdog++;
+  }
+
+  addRefri() {
+    this.qtdRefri++;
+  }
+
+  addBala() {
+    this.qtdBala++;
+  }
+
+  addChocolate() {
+    this.qtdChocolate++;
+  }
+
+  delHotdog() {
+    if (this.qtdHotdog > 0) this.qtdHotdog--;
+  }
+  delRefri() {
+    if (this.qtdRefri > 0) this.qtdRefri--;
+  }
+  delBala() {
+    if (this.qtdBala > 0) this.qtdBala--;
+  }
+  delChocolate() {
+    if (this.qtdChocolate > 0) this.qtdChocolate--;
+  }
 }
