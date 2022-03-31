0b22f96d0a52d46bbdf27b0a499e0b8978451de4
Igor Enrico <igorefp@gmail.com>
2022-03-30 22:25:20 -0300

    update app-cigarrometro

--- app-cigarrometro/src/app/home/home.page.ts
+++ app-cigarrometro/src/app/home/home.page.ts
@@ -9,6 +9,7 @@ export class HomePage {
   quantidadeCigarro: number = null;
   quantidadeAnoFumando: number = null;
   tempoVidaPerdido: number = 0;
+  tipoTempoPerdido: string = "Dia(s)!";
 
   constructor() {}
 
@@ -16,10 +17,21 @@ export class HomePage {
     const totalDiasPorAno = 360;
     const minutosPerdidoporCigarro = 10;
     const totalMinutosPorDia = 1440;
+    const totalDiasPorMes = 30;
+    const totalMesesPorAno = 12;
     let totalDiasFumando = this.quantidadeAnoFumando * totalDiasPorAno;
     let totalCigarroFumado = totalDiasFumando * this.quantidadeCigarro;
     this.tempoVidaPerdido = totalCigarroFumado * minutosPerdidoporCigarro;
     this.tempoVidaPerdido = this.tempoVidaPerdido / totalMinutosPorDia;
-
+    if (this.tempoVidaPerdido >= 30) 
+    {
+      this.tempoVidaPerdido = this.tempoVidaPerdido / totalDiasPorMes;
+      this.tipoTempoPerdido = "Mês(es)";
+      if (this.tempoVidaPerdido >= 12)
+      {
+        this.tempoVidaPerdido = this.tempoVidaPerdido / totalMesesPorAno;
+        this.tipoTempoPerdido = "Ano(s)";
+      }
+    }
   }
 }
