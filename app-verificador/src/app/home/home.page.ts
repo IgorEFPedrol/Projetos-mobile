0356504810831662d7cbf3766a7c8c32825fbcf8
Igor Enrico <igorefp@gmail.com>
2022-04-27 22:43:08 -0300

    update app-verificador

--- app-verificador/src/app/home/home.page.ts
+++ app-verificador/src/app/home/home.page.ts
@@ -9,6 +9,8 @@ export class HomePage {
 
   senha: string = '';
   pontuacao: number = 0;
+  frase: string[] = ['Muito Fraco', 'Fraco', 'Médio', 'Forte', 'Muito Forte']
+  imagens: string[] = ['', '../../assets/senha/1.png', '../../assets/senha/2.png', '../../assets/senha/3.png']
 
   constructor() {}
 
@@ -24,6 +26,16 @@ export class HomePage {
       this.pontuacao++;  
 
     if(this.senha.length > 8)   
-      this.pontuacao++;    
+      this.pontuacao++;
+      
+    if(this.pontuacao == 2)
+    document.getElementById('frase').style.color = "yellow";
+
+    if(this.pontuacao == 3)
+    document.getElementById('frase').style.color = "green";
+
+    if(this.pontuacao == 4)
+    document.getElementById('frase').style.color = "blue";
+    
   }
 }
