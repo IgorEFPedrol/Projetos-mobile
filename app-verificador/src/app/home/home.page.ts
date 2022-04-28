b2fae6c3ae757ae9ec2389ae113908deb15611e6
Igor Enrico <igorefp@gmail.com>
2022-04-27 22:47:20 -0300

    update app-verificador cor

--- app-verificador/src/app/home/home.page.ts
+++ app-verificador/src/app/home/home.page.ts
@@ -10,32 +10,36 @@ export class HomePage {
   senha: string = '';
   pontuacao: number = 0;
   frase: string[] = ['Muito Fraco', 'Fraco', 'Médio', 'Forte', 'Muito Forte']
-  imagens: string[] = ['', '../../assets/senha/1.png', '../../assets/senha/2.png', '../../assets/senha/3.png']
+  imagens: string[] = ['', '../../assets/senha/1.png', '../../assets/senha/2.png', '../../assets/senha/3.png',
+    '../../assets/senha/4.png'];
+  cor: string = 'red';
 
-  constructor() {}
+  constructor() { }
 
   verificarSenha() {
     this.pontuacao = 0;
-    if(this.senha.match(/[a-z]/))
-      this.pontuacao++;    
-
-    if(this.senha.match(/[A-Z]/))
-      this.pontuacao++;
-      
-    if(this.senha.match(/[0-9]/))
-      this.pontuacao++;  
-
-    if(this.senha.length > 8)   
-      this.pontuacao++;
-      
-    if(this.pontuacao == 2)
-    document.getElementById('frase').style.color = "yellow";
-
-    if(this.pontuacao == 3)
-    document.getElementById('frase').style.color = "green";
-
-    if(this.pontuacao == 4)
-    document.getElementById('frase').style.color = "blue";
+    this.cor = 'red';
     
+    if (this.senha.match(/[a-z]/))
+      this.pontuacao++;
+
+    if (this.senha.match(/[A-Z]/))
+      this.pontuacao++;
+
+    if (this.senha.match(/[0-9]/))
+      this.pontuacao++;
+
+    if (this.senha.length > 8)
+      this.pontuacao++;
+
+    if (this.pontuacao == 2)
+      this.cor = "yellow";
+
+    if (this.pontuacao == 3)
+      this.cor = "green";
+
+    if (this.pontuacao == 4)
+      this.cor = "blue";
+
   }
 }
