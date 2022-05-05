4ded0b9088362a7d864a8678996a931df37e5ed1
Igor Enrico <igorefp@gmail.com>
2022-05-04 22:20:20 -0300

    Update home.page.ts

--- app-tambler/src/app/home/home.page.ts
+++ app-tambler/src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  url = 'http://lucasreno.kinghost.net/frase';
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    http.get<any>(this.url)
+  }
+
+
 
 }
