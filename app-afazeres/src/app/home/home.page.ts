42f6f7ef9fd6c0350bb2b000c58cc90865689e1b
Igor Enrico <igorefp@gmail.com>
2022-05-11 22:20:52 -0300

    UPDATE app-afazeres

--- app-afazeres/src/app/home/home.page.ts
+++ app-afazeres/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { empty, EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tarefas: string[] = [
+    'Arrumar o quarto',
+    'Lavar a louça',
+    'Passar pano'
+  ];
+  novaTarefa: string = '';
 
   constructor() {}
 
+  cadastrarTarefa()
+  {
+    if (this.novaTarefa != '')
+    {
+    this.tarefas.push(this.novaTarefa);
+    this.novaTarefa = '';
+    }  
+  }
+
+  removerTarefa() 
+  {
+    
+  }
 }
