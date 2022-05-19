7614ef17baeed8d1e45e1b63efcd1320a1cbe507
Igor Enrico <igorefp@gmail.com>
2022-05-19 20:42:06 -0300

    UPDATE app-delivery

--- app-delivery/src/app/detalhe/detalhe.page.ts
+++ app-delivery/src/app/detalhe/detalhe.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Categoria } from '../interface/categoria';
+import { Produto } from '../interface/produto';
 
 @Component({
   selector: 'app-detalhe',
@@ -6,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalhe.page.scss'],
 })
 export class DetalhePage implements OnInit {
+  URL_BASE = 'http://lucasreno.kinghost.net/delivery';
+  dados: Categoria[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    
+  }
 
   ngOnInit() {
   }
--- app-delivery/src/app/home/home.page.ts
+++ app-delivery/src/app/home/home.page.ts
@@ -1,11 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Categoria } from '../interface/categoria';
+import { Produto } from '../interface/produto';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+
+registerLocaleData(ptBr);
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers:      [
+    { provide: LOCALE_ID, useValue: 'pt' },
+  ], 
 })
 export class HomePage {
   URL_BASE = 'http://lucasreno.kinghost.net/delivery';
@@ -19,6 +31,13 @@ export class HomePage {
     this.http.get<Categoria[]>(this.URL_BASE + '/categorias').subscribe(
       resposta => {
         this.dados = resposta;
+        this.dados.forEach(dado => {
+          this.http.get<Produto[]>(this.URL_BASE + '/produtos/' + dado.idCategoria).subscribe(
+            resp => {
+              dado.produtos = resp;
+            }
+          );
+        })
       }
     );
   }
