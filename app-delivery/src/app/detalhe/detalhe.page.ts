4ab59d9f514568c40457e02b79c46d0401027295
Igor Enrico <igorefp@gmail.com>
2022-05-18 22:02:59 -0300

    CREATE app-delivery

---
+++ app-delivery/src/app/detalhe/detalhe.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe',
+  templateUrl: './detalhe.page.html',
+  styleUrls: ['./detalhe.page.scss'],
+})
+export class DetalhePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
