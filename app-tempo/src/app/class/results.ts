e8e7b381d7c4ccd750646b15d8cad3c772686b37
Igor Enrico <igorefp@gmail.com>
2022-08-26 22:25:17 -0300

    UPDATE add data from API

--- app-tempo/src/app/class/results.ts
+++ app-tempo/src/app/class/results.ts
@@ -1,2 +1,20 @@
+import { Forecast } from "./forecast";
+
 export class Results {
+    temp: number;
+    date: string;
+    time: string;
+    condition_code: string;
+    description: string;
+    currently: string;
+    cid: string;
+    city: string;
+    img_id: string;
+    humidity: number;
+    wind_speedy: string;
+    sunrise: string;
+    sunset: string;
+    condition_slug: string;
+    city_name: string;
+    forecast: Forecast[];
 }
--- app-tempo/src/app/home/home.page.ts
+++ app-tempo/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Results } from '../class/results';
+import { Forecast } from '../class/forecast';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  results: Results = new Results();
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.results.forecast = [
+      new Forecast(),
+      new Forecast(),
+      new Forecast(),
+      new Forecast(),
+      new Forecast(),
+    ];
+  }
+
+  ngOnInit() {
+    const cors = 'http://cors-anywhere.herokuapp.com/';
+    const url = cors + 'https://api.hgbrasil.com/weather?woeid=457351'
+    this.http.get(url).subscribe(
+      response => this.results = response["results"]
+    );
+  }
+
 
 }
