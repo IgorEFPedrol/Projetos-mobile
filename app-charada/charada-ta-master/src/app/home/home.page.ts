d61dd5405627448e60fc24c9e786907b922b739b
Igor Enrico <igorefp@gmail.com>
2022-10-21 22:02:51 -0300

    update app-charada

--- app-charada/charada-ta-master/src/app/home/home.page.ts
+++ app-charada/charada-ta-master/src/app/home/home.page.ts
@@ -8,14 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   pergunta: string;
+  respostaPergunta: string;
   resposta: string;
   constructor(private http: HttpClient) {}
+  
+  ngOnInit(){
+    this.solicitarCharada();
+  }
 
   solicitarCharada(){
     const url = 'http://lucasreno.kinghost.net/charada';
     this.http.get(url).subscribe(resultado => {
       this.pergunta = resultado[0]["pergunta"];
-      this.resposta = resultado[0]["resposta"];
+      this.respostaPergunta = resultado[0]["resposta"];
+      this.resposta = "";
     });
+  }
+  solicitarResposta(){
+    this.resposta = this.respostaPergunta;
   }
 }
